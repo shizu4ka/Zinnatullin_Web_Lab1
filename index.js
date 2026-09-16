1331faf02ceffcbf794f629051a6ccbe36f4a400
Bulat Zinnatullin <bulat_zlinn@niuitmo.ru>
2026-09-16 15:53:34 +0300

    rename project

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Happy developing ✨')
+/*
+* math function
+when x>=-r & x<=-r/2:
+{
+    r^2>=y^2+x^2 & y<=0 & x<=-r/2
+}
+when x>=-r/2 & x<=0:
+{
+    r^2>=y^2+x^2 & y<=0 & x<=0 & x>=-r/2
+    x>=-r/2 & x<=0 & y>=0 & y<=r
+}
+when x>=0 & x<=r:
+{
+    y>=x-r & x>0 & y<0 & y>-r
+}
+*/
+
+function IsPointInArea(x,y,r){
+    if(x>=-r && x<=-r/2){
+        if(r**2>=y**2+x**2 && y<=0) return true;
+        else return false;
+    }
+    else if(x>=-r/2 && x<=0){
+        if(r**2>=y**2+x**2 && y<=0) return true;
+        else if(y>=0 && y<=r) return true;
+        else return false;
+    }
+    else if(x>=0 && x<=-r){
+        if(y>=x-r && y<=0 && y>=-r) return true;
+        else return false;
+    }
+    else return false;
+}
+
