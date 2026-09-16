d26e7d4d79355625173d92bb0c5d727b8148380e
Bulat Zinnatullin <bulat_zlinn@niuitmo.ru>
2026-09-16 18:37:47 +0300

    added validCheck and added a few html objects(spans for valid information)

--- index.js
+++ index.js
@@ -15,20 +15,53 @@ when x>=0 & x<=r:
 }
 */
 
-function IsPointInArea(x,y,r){
-    if(x>=-r && x<=-r/2){
-        if(r**2>=y**2+x**2 && y<=0) return true;
-        else return false;
-    }
-    else if(x>=-r/2 && x<=0){
-        if(r**2>=y**2+x**2 && y<=0) return true;
-        else if(y>=0 && y<=r) return true;
-        else return false;
-    }
-    else if(x>=0 && x<=-r){
-        if(y>=x-r && y<=0 && y>=-r) return true;
-        else return false;
-    }
-    else return false;
+function IsPointInArea(x, y, r) {
+    if (x >= -r && x <= -r / 2) {
+        return r ** 2 >= y ** 2 + x ** 2 && y <= 0;
+    } else if (x >= -r / 2 && x <= 0) {
+        if (r ** 2 >= y ** 2 + x ** 2 && y <= 0) return true;
+        else return y >= 0 && y <= r;
+    } else if (x >= 0 && x <= -r) {
+        return y >= x - r && y <= 0 && y >= -r;
+    } else return false;
 }
+
+//обработка формы
+let x = null;
+document.getElementById('X').addEventListener('click', function (event) {
+    if(event.target.tagName === 'BUTTON'){
+        x = event.target.textContent;
+    }
+})
+const form = document.querySelector("#formForPoint");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const y = document.getElementById('Y').value.trim();
+    //проверка валидации
+    const errorSpanY = document.getElementById('YError')
+    const errorSpanX = document.getElementById('XError')
+    errorSpanX.textContent = " ";
+    errorSpanY.textContent = " ";
+    if(x === null){
+        event.preventDefault();
+        errorSpanX.textContent = 'Please select X';
+        return;
+    }
+    if(!y){
+        event.preventDefault();
+        errorSpanY.textContent = 'Please enter a valid number';
+        return;
+    }
+    const num = Number(y.replace(',','.'))
+    if(isNaN(y) || num<-5 || num>5){
+        event.preventDefault();
+        errorSpanY.textContent = 'Please enter a valid number';
+        return;
+    }
+
+    const r = document.querySelector('input[name="OptionR"]:checked').value;
+    const result = IsPointInArea(x, y, r);
+    document.getElementById('output').textContent = result ? "true" : "false";
+});
+
 
